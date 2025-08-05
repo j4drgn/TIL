bfd6eee6bbd6f4f24963e9431f09319c3c9abc4a
j4drgn <kimyong0423@naver.com>
2025-08-05 16:57:29 +0900

    [Java] 멀티스레드 예제 코드 추가 - 스레드 생성 및 실행, Runnable 인터페이스, Thread 클래스 상속

---
+++ TECH/Java/multiThread/sec02/BeepTask.java
@@ -0,0 +1,21 @@
+package multiThread.sec02;
+
+import java.awt.Toolkit;
+
+public class BeepTask implements Runnable {
+
+  //beep음 출력 담당 - Runnable을 구현
+  @Override
+  public void run() {
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    //비프음 출력 - 작업 1
+    for (int i = 0; i < 5; i++) {
+      toolkit.beep(); //비프음 발샐
+      try {
+        Thread.sleep(500);
+      } catch (Exception e) {
+      }
+    }
+  }
+
+}
---
+++ TECH/Java/multiThread/sec03/BeepThread.java
@@ -0,0 +1,20 @@
+package multiThread.sec03;
+
+import java.awt.Toolkit;
+
+//thread 클래스 상속 - 객체 인스턴스가 thread가 됨.
+public class BeepThread extends Thread {
+
+  @Override
+  public void run() {
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    //비프음 출력 - 작업 1
+    for (int i = 0; i < 5; i++) {
+      toolkit.beep(); //비프음 발샐
+      try {
+        Thread.sleep(500);
+      } catch (Exception e) {
+      }
+    }
+  }
+}
