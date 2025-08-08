e49ab43ea0bb936611187e319fdf698c68a18157
j4drgn <kimyong0423@naver.com>
2025-08-08 13:17:53 +0900

    [SQL] 학습 파일 주석 추가 및 학습 내용 정리

--- TECH/SQL/03_DML_insert.sql
+++ TECH/SQL/03_DML_insert.sql
@@ -1,3 +1,4 @@
+
 --데이터 조적어(DML)
 -- 데이터 입력/삭제/수정/검색
 -- INSERT문 : 테이블에 데이터(튜플)을 저장하는 조작
@@ -8,6 +9,102 @@
 INSERT INTO STUDENT(STDNO, STDNAME, STDYEAR, DEPTNO)
 values ('2016001', '홍길동', 4, '1');
 
-SELECT *
-FROM STUDENT
-WHERE STDNO = '2016001';
+-- 값 문자열일때는 '' 표시
+
+--열 나열 없이 실제값만 나열 : 단, 모든 필드의 값이 다 나열되어야함.
+insert into STUDENT
+values ('2016006', '변학도', '4', '1');
+
+-------------------------------------------------
+
+--select EX
+select BOOKNAME
+from book
+where BOOKNO = '1';
+
+-- 여러 개의 DATA(튜플)을 저장 : insert all into 테이블명() values () into 테이블명... select * from daul;
+
+insert all
+    into book
+values ('6', '데이터베이스', 35000, '2021-05-12', '2')
+into book
+values ('7', '알고리즘', 35000, '2021-05-12', '1')
+select *
+from dual;
+
+/*
+** 시퀀스란
+오라클 데이터베이스 객체로 유일한 값의 일련변호 생성
+지정한 수치로 증가하거나 감소
+기본키에 자동 증가값을 사용할 때 유용
+최대 15개까지 생성 가능
+테이블과 독립적으로 저장 생성
+하나의 시퀀스 여러 테이블에서 사용 가능
+*/
+-- 시퀀스 생성 : create sequence 시퀀스명 옵션
+create sequence NO_SEQ
+    start with 1
+    increment by 1
+    maxvalue 10000
+    minvalue 1
+    nocycle;
+
+--  시퀀스 적용할 테이블 생성
+CREATE TABLE board
+(
+    bNo      NUMBER PRIMARY KEY,
+    bSubject VARCHAR2(30) NOT NULL,
+    bname    VARCHAR2(20) NOT NULL
+);
+
+
+-- 데이터 추가
+insert into Board
+values (NO_SEQ.nextval, '추석', '홍길동'); -- NO_SEQ.NEXTVAL : 현재값에서 증가분만큼 증가시켜서 반환하고 현재 반환된 값 저장
+insert into Board
+values (NO_SEQ.nextval, '미세먼지', '홍길동'); -- NO_SEQ.NEXTVAL : 현재값에서 증가분만큼 증가시켜서 반환하고 현재 반환된 값 저장
+insert into Board
+values (NO_SEQ.nextval, '휴가', '홍길동'); -- NO_SEQ.NEXTVAL : 현재값에서 증가분만큼 증가시켜서 반환하고 현재 반환된 값 저장
+
+
+select *
+from board;
+
+-- 시퀀스 값 검색
+select NO_SEQ.currval
+from dual;
+
+-- 시퀀스 수정
+alter sequence NO_SEQ
+    maxvalue 1000;
+
+-- 수정 결과 검색 : 구조에 대한 검색, 시퀀스 정보 저장 테이블 user_sequences
+select SEQUENCE_NAME, MAX_VALUE
+from USER_SEQUENCES;
+
+-- 시퀀스 삭제 : drop sequences 시퀀스명
+drop sequence NO_SEQ;
+
+-- 삭제 확인 -- 삭제 했으므로 반환되는 결과 내용은 없음, 테이블이 반환됨 ( 내용이 빈 테이블 반환)
+select SEQUENCE_NAME
+from USER_SEQUENCES;
+
+/*
+* DUAL테이블
+오라클 자체에서 제공되는 테이블
+1개의 행과 1개의 열만 있는 더미 테이블
+SYS 소요자만 모든 사용자가 사용할 수 있음
+간단한 함수 계산 결과값 확인할때 확인 ( 리턴 받을 수 있음)
+*/
+
+SELECt CURRENT_DATE
+from dual;
+
+--------------------------------------
+-- 데이터 임포트
+-- 텍스트 파일 읽어서 테이블 생성 및 데이터 구성
+
+-- 데이터 임포트 후 제약 조건 추가
+ALTER TABLE PRODUCT
+    ADD CONSTRAINT PK_PRODUCT_PRDN01
+        primary key (PRDNO);
--- TECH/SQL/ALTER_EX.sql
+++ TECH/SQL/ALTER_EX.sql
@@ -1,3 +1,5 @@
+
+-- ALTER 문과 INSERT 문 연습 문제 풀이 파일
 -- 테이블 ALTER 연습문제
 
 -- 1. product 테이블에 숫자 값을 갖는 prdStock과 제조일을 나타내는 prdDate 열 추가
@@ -17,3 +19,18 @@ ALTER TABLE publisher
 -- 4. publisher 테이블에서 pubPhone 열 삭제
 ALTER TABLE publisher
     DROP COLUMN pubPhone;
+
+-- 테이블 Insert 연습문제 1
+INSERT ALL
+    INTO STUDENT (STDNO, STDNAME, STDYEAR, STDBIRTHDAY, DEPTNO)
+VALUES ('2016001', '홍길동', 4, '1997-01-01', '1')
+INTO STUDENT (STDNO, STDNAME, STDYEAR, STDBIRTHDAY, DEPTNO)
+VALUES ('2015002', '성춘향', 3, '1996-12-10', '3')
+INTO STUDENT (STDNO, STDNAME, STDYEAR, STDBIRTHDAY, DEPTNO)
+VALUES ('2014004', '이몽룡', 2, '1996-03-03', '2')
+INTO STUDENT (STDNO, STDNAME, STDYEAR, STDBIRTHDAY, DEPTNO)
+VALUES ('2016002', '변학도', 4, '1995-05-07', '1')
+INTO STUDENT (STDNO, STDNAME, STDYEAR, STDBIRTHDAY, DEPTNO)
+VALUES ('2015003', '수홍미', 3, '1997-11-11', '2')
+SELECT *
+FROM dual;
